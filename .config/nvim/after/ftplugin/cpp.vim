eb3fd276a99f6218464f08d5dd902e8f79678659
RoyStegeman <roystegeman@live.nl>
2023-12-24 20:19:08 +0100

    move tab-space relation to ftplugin from global

--- .config/nvim/after/ftplugin/cpp.vim
+++ .config/nvim/after/ftplugin/cpp.vim
@@ -1,1 +1,5 @@
-setlocal colorcolumn=100
+setlocal colorcolumn=100 " add visual vertical column to 
+setlocal tabstop=2 "show tab as 4 spaces
+setlocal shiftwidth=2 " make tab equivalen to 4 spaces
+setlocal expandtab=true " only use space characters, never tabs
+
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -9,10 +9,6 @@ vim.o.relativenumber = true -- show relative line nubmers (wich 0 being absolute
 
 vim.o.signcolumn = 'number' -- show signs in the numbers column
 
-vim.o.tabstop = 4 -- show tab as 4 spaces
-vim.o.shiftwidth = 4 -- make tab equivalen to 4 spaces
-vim.o.expandtab = true -- only use space characters, never tabs
-
 vim.o.updatetime = 300 -- miliseconds between tping a key and vim updating internal state (e.g. for syntax highlighting)
 
 vim.o.termguicolors = true -- enable true colors in terminal
