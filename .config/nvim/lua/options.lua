351b1c39bddc4e911d9e694bfd7b8db97f91f6e2
RoyStegeman <roystegeman@live.nl>
2023-12-24 20:11:45 +0100

    update colorcolumn for cpp

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,19 +2,22 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus' -- allow vim to interact with system clipboard
 
-vim.o.number = true
-vim.o.relativenumber = true
+vim.o.number = true -- show line number
+vim.o.relativenumber = true -- show relative line nubmers (wich 0 being absolute due to the previous line)
 
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = 'number' -- show signs in the numbers column
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 4 -- show tab as 4 spaces
+vim.o.shiftwidth = 4 -- make tab equivalen to 4 spaces
+vim.o.expandtab = true -- only use space characters, never tabs
 
-vim.o.updatetime = 300
+vim.o.updatetime = 300 -- miliseconds between tping a key and vim updating internal state (e.g. for syntax highlighting)
 
-vim.o.termguicolors = true
+vim.o.termguicolors = true -- enable true colors in terminal
 
-vim.o.mouse = 'a'
+vim.o.mouse = 'a' -- enables the mouse in all modes
+
+vim.o.list = true -- show visual for spaces
 
