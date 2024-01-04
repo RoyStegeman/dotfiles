4e2c55bc9488c242fea33421ae2900451671be21
RoyStegeman <roystegeman@live.nl>
2024-01-04 14:24:04 +0100

    astethics

--- .config/nvim/lua/pluginlist.lua
+++ .config/nvim/lua/pluginlist.lua
@@ -19,15 +19,15 @@ return {
     },
 
     {
-      "nvim-tree/nvim-tree.lua",
-      version = "*",
-      lazy = false,
-      dependencies = {
-        "nvim-tree/nvim-web-devicons",
-      },
-      config = function()
-        require("nvim-tree").setup {}
-      end,
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
     },
 
     {
